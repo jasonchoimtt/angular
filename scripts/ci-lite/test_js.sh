a933ea3e02ef63e75a7644d0f82d0d70a9d341e6
Jason Choi <jasonchoi@google.com>
2016-08-18 12:33:28 -0700

    bazel: debug karma launching in travis

--- scripts/ci-lite/test_js.sh
+++ scripts/ci-lite/test_js.sh
@@ -16,6 +16,8 @@ cd ../..
 
 
 echo 'travis_fold:start:test.unit'
+which chromium-browser chromium google-chrome google-chrome-stable || true
+env
 bazel --bazelrc=scripts/ci-lite/bazelrc test \
     :tool_tests :jasmine_tests :karma_test :router_karma_test \
     "--test_arg=--env=DISPLAY=${DISPLAY}" "--test_arg=--browsers=${KARMA_JS_BROWSERS}"
