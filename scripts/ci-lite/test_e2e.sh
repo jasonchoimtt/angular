85834791fac27b300b6551ae2d72c9dc082e3cc2
Jason Choi <jasonchoi@google.com>
2016-08-18 13:04:50 -0700

    bazel: test e2e in travis

--- scripts/ci-lite/test_e2e.sh
+++ scripts/ci-lite/test_e2e.sh
@@ -14,30 +14,25 @@ cd `dirname $0`
 source ./env.sh
 cd ../..
 
-echo 'travis_fold:start:test.e2e.build'
+echo 'travis_fold:start:test.e2e.node_modules_index'
 # Make sure build_defs/node_modules_index.bzl is up to date
 node build_defs/node_modules_indexer.js . build_defs/node_modules_index.bzl --verify
+echo 'travis_fold:end:test.e2e.node_modules_index'
 
-# Build everything first to improve parallelization
-bazel --bazelrc=scripts/ci-lite/bazelrc build \
-    :public_api_test :check_cycle_test :playground_test
-# ./scripts/ci-lite/offline_compiler_test.sh
-# ./tools/typings-test/test.sh
-echo 'travis_fold:end:test.e2e.build'
-
-echo 'travis_fold:start:test.e2e.misc'
-bazel --bazelrc=scripts/ci-lite/bazelrc test :public_api_test :check_cycle_test \
-    || true  # FIXME
-# ./scripts/ci-lite/offline_compiler_test.sh
-# ./tools/typings-test/test.sh
-echo 'travis_fold:end:test.e2e.misc'
-
-echo 'travis_fold:start:test.e2e.localChrome'
+echo 'travis_fold:start:test.e2e.bazel'
 if [[ ${TRAVIS} ]]; then
   sh -e /etc/init.d/xvfb start
 fi
-bazel --bazelrc=scripts/ci-lite/bazelrc run :playground_test
-echo 'travis_fold:end:test.e2e.localChrome'
+bazel --bazelrc=scripts/ci-lite/bazelrc test \
+    :public_api_test :check_cycle_test :playground_test \
+    "--test_arg=--env=DISPLAY=${DISPLAY}" "--test_arg=--env=CHROME_BIN=${CHROME_BIN}" \
+    || true  # FIXME
+echo 'travis_fold:end:test.e2e.bazel'
+
+echo 'travis_fold:start:test.e2e.misc'
+# ./scripts/ci-lite/offline_compiler_test.sh
+# ./tools/typings-test/test.sh
+echo 'travis_fold:end:test.e2e.misc'
 
 echo 'travis_fold:end:test.js'
 
