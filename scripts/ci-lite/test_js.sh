98ac1c6cf0d5a9ec514ed1b4a27d9bc1db3d1fc8
Jason Choi <jasonchoi@google.com>
2016-08-18 12:12:17 -0700

    bazel: try running karma in 'bazel test' in travis

--- scripts/ci-lite/test_js.sh
+++ scripts/ci-lite/test_js.sh
@@ -15,45 +15,10 @@ source ./env.sh
 cd ../..
 
 
-# echo 'travis_fold:start:test.unit.tools'
-
-# # Run unit tests in tools
-# node ./dist/tools/tsc-watch/ tools runCmdsOnly
-
-# echo 'travis_fold:end:test.unit.tools'
-
-
-echo 'travis_fold:start:test.unit.build'
-# Build everything first to improve parallelization
-bazel --bazelrc=scripts/ci-lite/bazelrc build :jasmine_tests :karma_test :router_karma_test
-echo 'travis_fold:start:test.unit.build'
-
-echo 'travis_fold:start:test.unit.node'
-
-# Run unit tests in node
-bazel --bazelrc=scripts/ci-lite/bazelrc test :jasmine_tests \
-    || true  # FIXME: remove
-
-echo 'travis_fold:end:test.unit.node'
-
-
-echo 'travis_fold:start:test.unit.localChrome'
-
-# rebuild to revert files in @angular/compiler/test
-# TODO(tbosch): remove this and teach karma to serve the right files
-# node dist/tools/@angular/tsc-wrapped/src/main -p modules/tsconfig.json
-
-# Run unit tests in local chrome
-if [[ ${TRAVIS} ]]; then
-  sh -e /etc/init.d/xvfb start
-fi
-
-# We have to use "run" so that Karma has access to local Chrome.
-bazel --bazelrc=scripts/ci-lite/bazelrc run :karma_test -- --browsers=${KARMA_JS_BROWSERS} \
-  || true  # FIXME: remove
-bazel --bazelrc=scripts/ci-lite/bazelrc run :router_karma_test -- --browsers=${KARMA_JS_BROWSERS}
-
-echo 'travis_fold:end:test.unit.localChrome'
-
+echo 'travis_fold:start:test.unit'
+bazel --bazelrc=scripts/ci-lite/bazelrc test \
+    :tool_tests :jasmine_tests :karma_test :router_karma_test \
+    "--test_arg=--env=DISPLAY=${DISPLAY}" "--test_arg=--browsers=${KARMA_JS_BROWSERS}"
+echo 'travis_fold:start:test.unit'
 
 echo 'travis_fold:end:test.js'
