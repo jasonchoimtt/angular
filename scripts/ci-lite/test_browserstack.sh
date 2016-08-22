07ccad943d18c8e1ddb8db318adf2b18a8965d9c
Jason Choi <jasonchoi@google.com>
2016-08-22 11:10:24 -0700

    try to fix saucelabs and browserstack

--- scripts/ci-lite/test_browserstack.sh
+++ scripts/ci-lite/test_browserstack.sh
@@ -18,14 +18,17 @@ cd ../..
 ./scripts/browserstack/waitfor_tunnel.sh
 export BROWSER_STACK_ACCESS_KEY=`echo $BROWSER_STACK_ACCESS_KEY | rev`
 
-bazel --bazelrc=scripts/ci-lite/bazelrc test \
-    :karma_test :router_karma_test \
-    --test_env=BROWSER_STACK_USERNAME \
-    --test_env=BROWSER_STACK_ACCESS_KEY \
-    --test_env=CI_MODE \
-    --test_env=TRAVIS \
-    --test_env=TRAVIS_BUILD_NUMBER \
-    --test_env=TRAVIS_BUILD_ID \
-    "--test_arg=--browsers=${KARMA_JS_BROWSERS}"
+for target in :karma_test :router_karma_test; do
+  bazel --bazelrc=scripts/ci-lite/bazelrc test \
+      $target \
+      --test_env=BROWSER_STACK_USERNAME \
+      --test_env=BROWSER_STACK_ACCESS_KEY \
+      --test_env=CI_MODE \
+      --test_env=TRAVIS \
+      --test_env=TRAVIS_BUILD_NUMBER \
+      --test_env=TRAVIS_BUILD_ID \
+      "--test_arg=--browsers=${KARMA_JS_BROWSERS}"
+done
+
 
 echo 'travis_fold:end:test_browserstack'
--- scripts/ci-lite/test_saucelabs.sh
+++ scripts/ci-lite/test_saucelabs.sh
@@ -18,14 +18,16 @@ cd ../..
 ./scripts/sauce/sauce_connect_block.sh
 SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
 
-bazel --bazelrc=scripts/ci-lite/bazelrc test \
-    :karma_test :router_karma_test \
-    --test_env=SAUCE_USERNAME \
-    --test_env=SAUCE_ACCESS_KEY \
-    --test_env=CI_MODE \
-    --test_env=TRAVIS \
-    --test_env=TRAVIS_BUILD_NUMBER \
-    --test_env=TRAVIS_BUILD_ID \
-    "--test_arg=--browsers=${KARMA_JS_BROWSERS}"
+for target in :karma_test :router_karma_test; do
+  bazel --bazelrc=scripts/ci-lite/bazelrc test \
+      $target \
+      --test_env=SAUCE_USERNAME \
+      --test_env=SAUCE_ACCESS_KEY \
+      --test_env=CI_MODE \
+      --test_env=TRAVIS \
+      --test_env=TRAVIS_BUILD_NUMBER \
+      --test_env=TRAVIS_BUILD_ID \
+      "--test_arg=--browsers=${KARMA_JS_BROWSERS}"
+done
 
 echo 'travis_fold:end:test_saucelabs'
