90ac18695779e598f2be2c2b859f6baf014bd4f1
Jason Choi <jasonchoi@google.com>
2016-08-22 10:53:21 -0700

    fix jasmine_helper after merge

--- modules/jasmine_helper.ts
+++ modules/jasmine_helper.ts
@@ -1,11 +1,11 @@
 require('source-map-support').install();
 require('zone.js/dist/zone-node.js');
 require('zone.js/dist/long-stack-trace-zone.js');
-require('zone.js/dist/proxy-zone.js');
+require('zone.js/dist/proxy.js');
 require('zone.js/dist/sync-test.js');
 require('zone.js/dist/async-test.js');
 require('zone.js/dist/fake-async-test.js');
-require('zone.js/dist/jasmine-patch');
+require('zone.js/dist/jasmine-patch.js');
 
 require('reflect-metadata/Reflect');
 
